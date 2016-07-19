a1833a178e95794ff3c735cb8beb2c19aad8472e
danielrheinbay <danielrheinbay@gmail.com>
2016-07-19 21:25:48 +0200

    Changed get_all_apps.sh to follow another layer of indirection on apkmirror.com

--- get_all_apps.sh
+++ get_all_apps.sh
@@ -20,13 +20,16 @@ function download_apk {
     curl -# -L "${APK_HOST}${1}" > "${TMP_APK_SITE}"
     FULL_NAME_APK=`xidel ${TMP_APK_SITE} -s --extract  '//h1/@title'`
     APK_VERSION=`echo ${FULL_NAME_APK} | sed 's#Mi Fit ##g'`
-    APK_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[@type="button" and matches(@class, "downloadButton")]/@href'`
+    DL_PAGE_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[@type="button" and matches(@class, "downloadButton")]/@href'`
     THIS_APK_DIR="${APK_DIR}${APK_VERSION}"
     APK_FILE="${THIS_APK_DIR}/base_${APK_VERSION}.apk"
     if [ -e ${APK_FILE} ]; then
         echo "APK ${APK_FILE} already exist - skipping downloading..."
     else
-        echo "Download ${FULL_NAME_APK} from ${APK_URL}"
+        echo "Fetching download link for ${FULL_NAME_APK} from ${APK_HOST}${DL_PAGE_URL}"
+	curl -# -L "${APK_HOST}${DL_PAGE_URL}" > "${TMP_APK_SITE}"
+	APK_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[matches(@data-google-vignette, "false")]/@href'`
+        echo "Download ${FULL_NAME_APK} from ${APK_HOST}${APK_URL}"
         mkdir -p "${THIS_APK_DIR}"
         curl -# -L "${APK_HOST}${APK_URL}" > "${APK_FILE}"
     fi
