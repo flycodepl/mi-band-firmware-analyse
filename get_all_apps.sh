21206b7d412da853c915e52fcd921cda98fbc3f2
Pawel Flis <p.flis@wb.com.pl>
2016-08-17 11:20:25 +0200

    add parameters for get_all_apps.sh

--- get_all_apps.sh
+++ get_all_apps.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-
-
 APK_HOST="http://www.apkmirror.com"
 APK_LIST="/uploads/?q=mi-fit"
 
@@ -14,6 +12,8 @@ TMP_DIR=".temp"
 TMP_LIST="${TMP_DIR}/apk_list"
 TMP_APK_SITE="${TMP_DIR}/apk_download_site"
 
+FORCE=1 # default 1 (false)
+UNZIP=1 # default 1 (false)
 
 function download_apk {    
     echo -e "\nDownload apk site"
@@ -23,20 +23,22 @@ function download_apk {
     DL_PAGE_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[@type="button" and matches(@class, "downloadButton")]/@href'`
     THIS_APK_DIR="${APK_DIR}${APK_VERSION}"
     APK_FILE="${THIS_APK_DIR}/base_${APK_VERSION}.apk"
-    if [ -e ${APK_FILE} ]; then
+    if [ -e ${APK_FILE} ] && [ ${FORCE} -eq 1 ]; then
         echo "APK ${APK_FILE} already exist - skipping downloading..."
-    else
+    elif [ ! -e ${APK_FILE} ] || [ ${FORCE} -eq 0 ]; then
         echo "Fetching download link for ${FULL_NAME_APK} from ${APK_HOST}${DL_PAGE_URL}"
-	curl -# -L "${APK_HOST}${DL_PAGE_URL}" > "${TMP_APK_SITE}"
-	APK_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[matches(@data-google-vignette, "false")]/@href'`
+		curl -# -L "${APK_HOST}${DL_PAGE_URL}" > "${TMP_APK_SITE}"
+		APK_URL=`xidel ${TMP_APK_SITE} -s --extract '//a[matches(@data-google-vignette, "false")]/@href'`
         echo "Download ${FULL_NAME_APK} from ${APK_HOST}${APK_URL}"
         mkdir -p "${THIS_APK_DIR}"
         curl -# -L "${APK_HOST}${APK_URL}" > "${APK_FILE}"
     fi
 
-    echo "Unzip base_${APK_VERSION}.apk"
-    do_unzip $APK_FILE "${THIS_APK_DIR}/unziped/"
-    copy_fw "${THIS_APK_DIR}/unziped" $APK_VERSION
+	if [ ${UNZIP} -eq 0 ]; then
+		echo "Unzip base_${APK_VERSION}.apk"
+		do_unzip $APK_FILE "${THIS_APK_DIR}/unziped/"
+		copy_fw "${THIS_APK_DIR}/unziped" $APK_VERSION
+	fi
     echo -e "Done for ${FULL_NAME_APK}\n"
 }
 
@@ -56,6 +58,32 @@ function copy_fw {
     echo "copy all firmwares to ${DIR_FOR_FW}"
     cp -n ${UNZIP_APK_DIR}/assets/*.fw $DIR_FOR_FW
 }
+
+function usage {
+	echo "Usage: $0 [-u | -f]"
+	echo -e "\t-u|--unzip - auto-unzip each APK and copy FW to separate directory"
+	echo -e "\t-f|--force - Download APK even when corresponding file for this version already exist"
+}
+
+while [[ $# -gt 0 ]]; do
+	case $1 in
+		-u|--unzip)
+			UNZIP=0 # set to 0 (true)
+			shift
+			;;
+		-f|--force)
+			FORCE=0 # set to 0 (true)
+			shift
+			;;
+		*)
+			usage
+			exit
+			;;
+	esac
+
+done
+[ ${FORCE} -eq 0 ] && echo -e "FORCE is \e[4menabled\e[24m" || echo -e "FORCE is disabled - for enable, set --force"
+[ ${UNZIP} -eq 0 ] && echo -e "UNZIP is \e[4menabled\e[24m" || echo -e "UNZIP is disabled - for enable, set --unzip"
 
 mkdir -p "${TMP_DIR}"
 echo "Downloading APK list" 
